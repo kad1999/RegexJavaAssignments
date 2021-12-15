c26d14987c253c6ab1f4fa2aa95b885c925d53ca
KAD99 <anudak99@gmail.com>
2021-12-15 13:14:48 +0530

    Added coding assignment question 5 and mysql assignments-1,2

--- Coding-Assignments/src/cc5.java
+++ Coding-Assignments/src/cc5.java
@@ -1,6 +1,32 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
 
 public class cc5 {
+	public static List<String> letterCombinations(String digits) {
+		LinkedList<String> ans = new LinkedList<String>();
+		if(digits.isEmpty()) return ans;
+		String[] mapping = new String[] {"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+		ans.add("");
+		while(ans.peek().length()!=digits.length()){
+			String remove = ans.remove();
+			String map = mapping[digits.charAt(remove.length())-'0'];
+			for(char c: map.toCharArray()){
+				ans.addLast(remove+c);
+			}
+		}
+		return ans;
+	}
+	
 	public static void main(String[] args) {
+		System.out.println("Enter input string");
+		Scanner sc = new Scanner(System.in);
+		
+		String digits = sc.next();
+		List<String> result = letterCombinations(digits);
+		
+		System.out.println(result.toString());
 		
 	}
 }
